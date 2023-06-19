5a3b49d9de39e655fbfbb8c19dd4c032e48f9e4a
modfayz <modfayz@gmail.com>
2023-06-19 12:36:59 +0100

    adding pagination and search

--- app/Http/Livewire/Films.php
+++ app/Http/Livewire/Films.php
@@ -7,12 +7,26 @@ use App\Models\Genre;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Films extends Component
 {
+    use WithPagination;
+
+    public $q;
     public function render()
     {   
-        $films = Film::with('genres')->get();
+        $films = Film::with('genres')
+        ->when($this->q , function($query){
+            return $query->where(function($query){
+                return $query->where('title','like','%'.$this->q.'%')
+                            ->orWhere('overview','like','%'.$this->q.'%');
+            });
+        })
+        ->paginate(5);
         return view('livewire.films',['films' => $films]);
+    }
+    function updatingQ() {
+        $this->resetPage();
     }
 }
--- resources/views/livewire/films.blade.php
+++ resources/views/livewire/films.blade.php
@@ -1,6 +1,18 @@
 <div>
     
 <div class="relative overflow-x-auto">
+    
+    <div class="m-6 flex justify-between">
+        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+        <div class="relative">
+            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+            </div>
+            <input type="search" wire:model="q" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required>
+        
+        </div>
+    </div>
+
     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
@@ -11,10 +23,10 @@
                     Genre
                 </th>
                 <th scope="col" class="px-6 py-3">
-                    
+                    Release date
                 </th>
                 <th scope="col" class="px-6 py-3">
-                    Release date
+                    Action
                 </th>
             </tr>
         </thead>
@@ -22,7 +34,7 @@
            @foreach ($films as $film)
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    {{ Str::limit($film->title, 15) }}
+                    {{ Str::limit($film->title, 25) }}
 
                 </th>
                 <td class="px-6 py-4">
@@ -33,11 +45,16 @@
                 <td class="px-6 py-4">
                     {{$film->release_date}}
                 </td>
-               
+                <td class="px-6 py-4">
+                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Voire</button>
+                </td>
             </tr>
            @endforeach
         </tbody>
     </table>
+    <div class=" mt-6 mb-6   mx-6">
+        {{ $films->links() }}
+    </div>
 </div>
 
 </div>
